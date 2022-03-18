6b01e31143eebadfb9c162b26a18165ac14e76dd
wprzybytek <wojciechprzybytek@gmail.com>
2022-03-18 10:13:00 +0100

    ehhhhhhh

--- SysOp/lab1/zad2/test.c
+++ SysOp/lab1/zad2/test.c
@@ -5,22 +5,10 @@
 #include <unistd.h>
 
 #ifdef DYNAMIC
-#include <dlfcn.h>
+    #include <dlfcn.h>
 #else
-#include "wclibrary.h"
+    #include "wclibrary.h"
 #endif
-
-struct tms tms_start, tms_end;
-clock_t clock_start, clock_end;
-
-#define test_time(operation_name, operation)\
-  clock_start = times(&tms_start);\
-  operation\
-  clock_end = times(&tms_end);\
-  printf("%s - ", operation_name);\
-  printf("real time: %f, ", (double) (clock_end - clock_start) / (double) sysconf(_SC_CLK_TCK));\
-  printf("system time: %f, ", (double) (tms_end.tms_stime - tms_start.tms_stime) / (double) sysconf(_SC_CLK_TCK));\
-  printf("user time: %f\n", (double) (tms_end.tms_utime - tms_start.tms_utime) / (double) sysconf(_SC_CLK_TCK))
 
 int main(int argc, char** argv) {
     #ifdef DYNAMIC
@@ -36,6 +24,12 @@ int main(int argc, char** argv) {
         int (*save_to_array)(void) = (int (*)(void)) dlsym(handle, "save_to_array");
         void (*remove_block)(int) = (void (*)(int)) dlsym(handle, "remove_block");
     #endif
+
+    struct tms tms_start;
+    struct tms tms_end;
+    clock_t real_start;
+    clock_t real_end;
+
     int i = 2;
     while(1) {
 
@@ -48,29 +42,51 @@ int main(int argc, char** argv) {
             char** files = (char**)calloc(files_number, sizeof(char*));
             for(int j = 0; j < files_number; j++) {
                 files[j] = (char*)calloc(strlen(argv[i + 2 + j]), sizeof(char));
-                files[j] = argv[i + 2 + j];
+                strcat(files[j], argv[i + 2 + j]);
             }
+
+            real_start = times(&tms_start);
+            read_from_files(files, files_number);
+            real_end = times(&tms_end);
+            printf("wc files - ");
+            printf("real time: %f, ", (double) (real_end - real_start) / sysconf(_SC_CLK_TCK));
+            printf("system time: %f, ", (double) (tms_end.tms_cstime - tms_start.tms_cstime) / sysconf(_SC_CLK_TCK));
+            printf("user time: %f\n", (double) (tms_end.tms_cutime - tms_start.tms_cutime) / sysconf(_SC_CLK_TCK));
+
+            for(int j = 0; j < files_number; j++) {
+                free(files[j]);
+            }
+            free(files);
             i += 2 + files_number;
-            test_time("wc_files", {
-                read_from_files(files, files_number);
-            });
         }
         else if(strcmp(argv[i], "remove_block") == 0) {
-            test_time("remove_block", {
-                remove_block(atoi(argv[i + 1]));
-            });
+            real_start = times(&tms_start);
+            remove_block(atoi(argv[i + 1]));
+            real_end = times(&tms_end);
+            printf("remove block - ");
+            printf("real time: %f, ", (double) (real_end - real_start) / sysconf(_SC_CLK_TCK));
+            printf("system time: %f, ", (double) (tms_end.tms_cstime - tms_start.tms_cstime) / sysconf(_SC_CLK_TCK));
+            printf("user time: %f\n", (double) (tms_end.tms_cutime - tms_start.tms_cutime) / sysconf(_SC_CLK_TCK));
             i += 2;
         }
         else if(strcmp(argv[i], "testing") == 0) {
             char** file = (char**)calloc(1, sizeof(char*));
             file[0] = (char*)calloc(strlen(argv[i + 1]), sizeof(char));
-            file[0] = argv[i + 1];
-            test_time("add and remove big file 20 times", {
-                for(int k = 0; k < 100; k++) {
-                    int h = read_from_files(file, 1);
-                    remove_block(h);
-                }
-            });
+            strcat(file[0], argv[i + 1]);
+
+            real_start = times(&tms_start);
+            for(int k = 0; k < 100; k++) {
+                int h = read_from_files(file, 1);
+                remove_block(h);
+            }
+            real_end = times(&tms_end);
+            printf("add and delete big file 100 times - ");
+            printf("real time: %f, ", (double) (real_end - real_start) / sysconf(_SC_CLK_TCK));
+            printf("system time: %f, ", (double) (tms_end.tms_cstime - tms_start.tms_cstime) / sysconf(_SC_CLK_TCK));
+            printf("user time: %f\n", (double) (tms_end.tms_cutime - tms_start.tms_cutime) / sysconf(_SC_CLK_TCK));
+
+            free(file[0]);
+            free(file);
             i += 2;
         }
         else if(strcmp(argv[i], "end") == 0){
