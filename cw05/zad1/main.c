c026b249ad5a5912e94b34d09224a246a6e0e0d6
wprzybytek <wojciechprzybytek@gmail.com>
2022-04-15 15:57:26 +0200

    zad 1 done

--- cw05/zad1/main.c
+++ cw05/zad1/main.c
@@ -5,11 +5,17 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+// stored commands from the file, 1 dimension is line, 2 command, 3 argument, 4 character of argument
 char commands[5][5][5][100];
+// stored file content
+char *file_content;
 
-char* read_file(char* filepath) {
-    FILE* file = fopen(filepath, "r");
-    if(file == NULL) {
+// read file with commands that was provided as an argument as store its content in global array
+void read_file(char *filepath)
+{
+    FILE *file = fopen(filepath, "r");
+    if (file == NULL)
+    {
         printf("File does not exist.\n");
         exit(-1);
     }
@@ -18,40 +24,46 @@ char* read_file(char* filepath) {
     long size = ftell(file);
     fseek(file, 0, SEEK_SET);
 
-    char* content = (char*)malloc(sizeof(char) * size + 1);
-    fread(content, size, 1, file);
-    content[size] = '\0';
+    file_content = (char *)malloc(sizeof(char) * size + 1);
+    fread(file_content, size, 1, file);
+    file_content[size] = '\0';
     fclose(file);
-
-    return content;
 }
 
-void process_content(char* file_content) {
+// read the file content to get all the commands and store it in global array
+void process_content()
+{
     int line = 0, cmd = 0, arg = 0, letter = 0;
     int i = 13;
-    while(1) {
-        if(file_content[i] == '\n' && file_content[i + 1] == '\n') {
+    while (1)
+    {
+        if (file_content[i] == '\n' && file_content[i + 1] == '\n')
+        {
             return;
         }
-        else if(file_content[i] == '\n') {
+        else if (file_content[i] == '\n')
+        {
             i += 14;
             line += 1;
             cmd = 0;
             arg = 0;
             letter = 0;
         }
-        else if(file_content[i] == ' ' && file_content[i + 1] == '|') {
+        else if (file_content[i] == ' ' && file_content[i + 1] == '|')
+        {
             i += 3;
             cmd += 1;
             arg = 0;
             letter = 0;
         }
-        else if(file_content[i] == ' ') {
+        else if (file_content[i] == ' ')
+        {
             i += 1;
             arg += 1;
             letter = 0;
         }
-        else {
+        else
+        {
             commands[line][cmd][arg][letter] = file_content[i];
             letter += 1;
             i += 1;
@@ -59,63 +71,100 @@ void process_content(char* file_content) {
     }
 }
 
-int get_cmd(char line[5][5][100]) {
+// get number of commands in one line
+int get_cmd(char line[5][5][100])
+{
     int cnt = 0;
-    while(1) {
-        if(line[cnt][0][0] != '\0') {
+    while (1)
+    {
+        if (line[cnt][0][0] != '\0')
+        {
             cnt += 1;
         }
-        else {
+        else
+        {
             return cnt;
         }
     }
 }
 
-void run_line(int queue[25], int size) {
-    int no_commands = 0;
-    for(int i = 0; i < size; i++) {
-        no_commands += get_cmd(commands[queue[i]]);
+// get number of arguments in one command
+int get_arg(char cmd[5][100])
+{
+    int cnt = 0;
+    while (1)
+    {
+        if (cmd[cnt][0] != '\0')
+        {
+            cnt += 1;
+        }
+        else
+        {
+            return cnt;
+        }
     }
-    int pipes[no_commands - 1][2];
-    for(int i = 0; i < no_commands - 1; i++) {
-        pipe(pipes[i]);
+}
+
+// run all the commands from one sequence
+void run_sequence(int queue[25], int size)
+{
+    int no_commands = 0;
+    for (int i = 0; i < size; i++)
+    {
+        no_commands += get_cmd(commands[queue[i]]);
     }
 
     int curr = 0;
-    for(int i = 0; i < size; i++) {
+    int pipes[2], prev_pipes[2];
+    for (int i = 0; i < size; i++)
+    {
         int curr_line = queue[i];
-        for(int j = 0; j < get_cmd(commands[curr_line]); j++) {
-            pid_t pid = fork();
-            if(!pid) {
-                if (curr != no_commands - 1) {
-                    dup2(pipes[curr][1], STDOUT_FILENO);
-//                    close(pipes[curr][0]);
-                }
-                if (curr != 0) {
-                    dup2(pipes[curr - 1][0], STDIN_FILENO);
-//                    close(pipes[curr - 1][1]);
-                }
-//                for(int k = 0; k < no_commands - 1; k++) {
-//                    if (k != curr && k != curr - 1) {
-//                        close(pipes[k][1]);
-//                        close(pipes[k][0]);
-//                    }
-//                }
-                execlp(commands[curr_line][j][0], commands[curr_line][j][0], commands[curr_line][j][1], NULL);
-                printf("chuj\n");
+        for (int j = 0; j < get_cmd(commands[curr_line]); j++)
+        {
+
+            int args = get_arg(commands[curr_line][j]);
+            char **curr_command = (char **)malloc((args + 1) * sizeof(char *));
+            for (int k = 0; k < args; k++)
+            {
+                curr_command[k] = commands[curr_line][j][k];
             }
+            curr_command[args] = NULL;
+
+            pipe(pipes);
+            pid_t pid = fork();
+            if (!pid)
+            {
+                if (curr != 0)
+                {
+                    dup2(prev_pipes[0], STDIN_FILENO);
+                    close(prev_pipes[1]);
+                }
+                if (curr != no_commands - 1)
+                {
+                    dup2(pipes[1], STDOUT_FILENO);
+                }
+                execvp(curr_command[0], curr_command);
+                printf("exec not working\n");
+            }
+
+            close(pipes[1]);
+            prev_pipes[0] = pipes[0];
+            prev_pipes[1] = pipes[1];
             curr += 1;
+
+            free(curr_command);
         }
     }
-//    for (int i = 0; i < no_commands; ++i) {
-//        wait(0);
-//    }
 }
 
-void get_lines(char* file_content) {
+// process the rest of the file content to get all of the command sequences and run them
+void get_lines()
+{
     int index = 3;
-    while(1) {
-        if(file_content[index - 2] == '\n' && file_content[index - 1] == '\n') {
+    while (1)
+    {
+        if (file_content[index - 2] == '\n' && file_content[index - 1] == '\n')
+        {
             break;
         }
         index += 1;
@@ -124,32 +173,39 @@ void get_lines(char* file_content) {
     int queue[25];
     int size = 0;
 
-    while(1) {
-        if(file_content[index] == '\0') {
+    while (1)
+    {
+        if (file_content[index] == '\0')
+        {
             break;
         }
-        else if(file_content[index] == '\n') {
-            run_line(queue, size);
+        else if (file_content[index] == '\n')
+        {
+            run_sequence(queue, size);
+            while (wait(NULL) > 0)
+                ;
             size = 0;
         }
-        else if(file_content[index] - '0' > 0 && file_content[index] - '0' < 10) {
+        else if (file_content[index] - '0' > 0 && file_content[index] - '0' < 10)
+        {
             queue[size] = file_content[index] - '1';
             size += 1;
         }
         index += 1;
     }
-
 }
 
-int main(int argc, char** argv) {
-    if(argc != 2) {
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         printf("Enter file path.\n");
         exit(-1);
     }
 
-    char* file_content = read_file(argv[1]);
-    process_content(file_content);
-    get_lines(file_content);
+    read_file(argv[1]);
+    process_content();
+    get_lines();
 
     free(file_content);
     return 0;
